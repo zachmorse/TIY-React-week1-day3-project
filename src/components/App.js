d35e4ae0da721dd2c87e5727371bf9ce2ae8a8c5
Zach Morse <zacharymorse@gmail.com>
2017-07-19 14:35:18 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">Earthquakes!</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
